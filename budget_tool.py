cd3d4f64f3b3fb41c706c6c6fba014c3e5aaf67e
Grayson Sinclair <highwind360@gmail.com>
2016-09-30 18:46:31 -0700

    Retabbed and refactored budget tool.

--- budget_tool.py
+++ budget_tool.py
@@ -16,25 +16,25 @@ FINISH_DAY = 66
 
 
 def calculate(balance):
-	"""
-	Uses balance and the current day to figure out current daily budget
-	"""
-	# calculate the current day of year
-	current_time = datetime.now()
-	day = (current_time.month * 12) + current_time.day
+    """
+    Uses balance and the current day to figure out current daily budget
+    """
+    # calculate the current day of year
+    current_time = datetime.now()
+    day = (current_time.month * 12) + current_time.day
 
-	return balance / (FINISH_DAY - day)
+    return balance / (FINISH_DAY - day)
 
 def main():
-	# pass relevant arguments with argparse
-	parse = ArgumentParser(description="A tool for calculating how " + 
-									   "much to spend in a day")
-	parse.add_argument('money',type=int,default=0)
-	args = parse.parse_args()
+    # pass relevant arguments with argparse
+    parse = ArgumentParser(description="A tool for calculating how " + 
+                                       "much to spend in a day")
+    parse.add_argument('money',type=int,default=0)
+    args = parse.parse_args()
 
-	# calculate budget and either print or return
-	return calculate(args.money)
+    # calculate budget and print
+    print(calculate(args.money))
 
 
 if __name__ == "__main__":
-	print(main())
+    main()
