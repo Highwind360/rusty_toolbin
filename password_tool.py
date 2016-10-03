d22d6abb9dc3a4f1a5605d3de4d1a1121bf2e466
Grayson Sinclair <highwind360@gmail.com>
2016-10-03 15:18:00 -0700

    Retabbed the password tool.

--- password_tool.py
+++ password_tool.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 """
-	password_tool.py
-	Grayson Sinclair
+    password_tool.py
+    Grayson Sinclair
 
-	Generates a random password for the user.
-	TODO: Use argparse for arguments
-	TODO: generate random password
-	TODO: add options...
-		...for password details:
-			- include punctuation
-			- only lowercase
-			- only uppercase
-			- no numbers
-		...output password to file
-		...take username for replacing default username in file
-		...batch usernames/passwords
-	TODO: use sqlite to create database for password management
-		- Make database encrypted w/password management
+    Generates a random password for the user.
+    TODO: Use argparse for arguments
+    TODO: generate random password
+    TODO: add options...
+        ...for password details:
+            - include punctuation
+            - only lowercase
+            - only uppercase
+            - no numbers
+        ...output password to file
+        ...take username for replacing default username in file
+        ...batch usernames/passwords
+    TODO: use sqlite to create database for password management
+        - Make database encrypted w/password management
 """
 
 from sys import argv
@@ -25,23 +25,52 @@ from string import printable
 from random import randrange
 
 
-# TODO: move password generation to its own function
-def main(pwd_length=12):
-	password, charset = '', ''
-	if True: # TODO: add conditions to add only desired chars to charset
-		charset += printable[:-38]
-	
-	for i in range(pwd_length):
-		pick_index = randrange(len(charset))
-		password += charset[pick_index]
-	
-	print(password)
+def password_generator(charset, length):
+    """The password generation function.
+
+    This takes a set of characters and
+    composes a random password out of them.
+    """
+    password = ''
+
+    for i in range(length):
+        pick_index = randrange(len(charset))
+        password += charset[pick_index]
+
+    return password
+
+def compose_charset(options):
+    """Chooses a set of characters to generate a password with.
+
+    The user can specifify a series of options for sets of characters to add to
+    the overall character set. If nothing is specified, a default set of
+    characters are included, instead.
+    """
+    charset = ''
+
+    if False: # TODO: add conditions to add only desired chars to charset
+        pass
+    else:
+        # the default set of characters
+        charset += printable[:-38]
+
+    return charset
+
+def main(pwd_length=12, charset_options=[]):
+    """The main function.
+
+    pwd_length - the length of the password to generate as an int.
+    charset_options - a list of character sets to add to the master charset.
+    """
+    charset = compose_charset(charset_options)
+    print(password_generator(charset, pwd_length))
 
 
 if __name__ == "__main__":
-	if len(argv) > 2:
-		print("Usage: {name} pwd_length".format(name = argv[0]))
-	elif len(argv) == 2:
-		main(int(argv[1]))
-	else:
-		main()
+    # TODO: handle arguments with argparse
+    if len(argv) > 2:
+        print("Usage: {name} pwd_length".format(name = argv[0]))
+    elif len(argv) == 2:
+        main(int(argv[1]))
+    else:
+        main()
