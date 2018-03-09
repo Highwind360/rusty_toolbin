7ce01c8f221771087ee11e35db29df6742a3c454
Grayson Sinclair <highwind360@gmail.com>
2018-03-09 13:00:27 -0800

    Efficiency improvement for megamult.

--- megamultiplier.py
+++ megamultiplier.py
@@ -3,7 +3,7 @@
 megamultiplier.py
 highwind
 
-Factorizes an integer argument
+Prints the product of all divisors of a given N
 """
 from sys import argv
 from functools import reduce
@@ -11,8 +11,8 @@ from itertools import combinations
 
 # Like sum, but multiplication
 mult = lambda things: reduce(lambda x,y: x*y, things)
-# Turns a 2D array into a 1D array
-flatten = lambda l: [item for sublist in l for item in sublist]
+# Turns a 3D array into a 1D array
+smash = lambda l: [item for sublist in l for two_d_list in sublist for item in two_d_list]
 
 # Credit: https://stackoverflow.com/a/16996439
 def factorize_primes(n):
@@ -34,7 +34,7 @@ def main():
 
     factors = factorize_primes(int(argv[1]))
     # multiplies every possible combination of 'factors' together
-    print(mult(map(mult, flatten([combinations(factors, i) for i in range(1, len(factors)+1)]))))
+    print(mult(smash([combinations(factors, i) for i in range(1, len(factors)+1)])))
 
 if __name__ == "__main__":
     main()
